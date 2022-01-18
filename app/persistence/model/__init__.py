3568da771291678a89e8ba05214570733ac9ca85
Ekajsa <kajsa.einarsson@studerande.plushogskolan.se>
2022-01-18 13:12:39 +0100

    Project structure set up, but init_db is not working

---
+++ app/persistence/model/__init__.py
@@ -0,0 +1,5 @@
+# from app.persistence.db import Document, db
+#
+#
+# class User(Document):
+#     collection = db.users
---
+++ app/persistence/repository/user_repository.py
@@ -0,0 +1,5 @@
+# from app.persistence.model import User
+#
+#
+# def get_all_users():
+#     return User.all()
