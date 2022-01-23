1fa590ae9d18fa1f97cdc2611d9449882d6ac0aa
JohannaUlveklint <johanna.ulveklint@gmail.com>
2022-01-23 22:57:17 +0100

    Worked with Select and Continue buttons

--- app/blueprints/user/__init__.py
+++ app/blueprints/user/__init__.py
@@ -4,18 +4,23 @@ from flask_login import login_required
 from app.controllers import user_controller as uc
 from app.controllers import event_controller as ec
 
-bp_user = Blueprint('bp_user', __name__)
+bp_user = Blueprint("bp_user", __name__)
 
 
-@bp_user.get('/')
+@bp_user.get("/")
 def user():
     # De-comment to create database posts!
     # uc.create_user()
     # ec.create_event()
-    return render_template('user.html')
+    return render_template("user.html")
 
 
-@bp_user.get('/firstsorting')
+@bp_user.get("/disciplines")
 # @login_required
-def first_sorting_get():
-    return render_template('selectdisciplines.html')
+def select_disciplines_get():
+    return render_template("selectdisciplines.html")
+
+
+@bp_user.get("/countries")
+def select_countries_get():
+    return render_template("selectcountries.html")
