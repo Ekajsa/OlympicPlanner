5c4df76f5bc48e66c499ecd9fe3ebf577d42ecf2
JohannaUlveklint <johanna.ulveklint@gmail.com>
2022-01-25 14:28:09 +0100

    Changed the select all to a new one since the old did not work, neither does the new one, will ask Joakim

--- app/blueprints/user/__init__.py
+++ app/blueprints/user/__init__.py
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
-from flask import Blueprint, render_template
-from flask_login import login_required
+
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, logout_user, current_user
 
 from app.controllers import user_controller as uc
 from app.controllers import event_controller as ec
-=======
-from flask import Blueprint, redirect, url_for, render_template
-from flask_login import logout_user, current_user
->>>>>>> 43096225245274b6abc58310eb08575887961957
+
 
 bp_user = Blueprint("bp_user", __name__)
 
 
-<<<<<<< HEAD
 @bp_user.get("/")
 def user():
     # De-comment to create database posts!
@@ -21,16 +17,6 @@ def user():
     return render_template("user.html")
 
 
-@bp_user.get("/disciplines")
-# @login_required
-def select_disciplines_get():
-    return render_template("selectdisciplines.html")
-
-
-@bp_user.get("/countries")
-def select_countries_get():
-    return render_template("selectcountries.html")
-=======
 @bp_user.before_request
 def before_request():
     if not current_user.is_authenticated:
@@ -47,4 +33,14 @@ def signout_get():
 # @login_required
 def schedules_get():
     return render_template("schedules.html")
->>>>>>> 43096225245274b6abc58310eb08575887961957
+
+
+@bp_user.get("/disciplines")
+# @login_required
+def select_disciplines_get():
+    return render_template("selectdisciplines.html")
+
+
+@bp_user.get("/countries")
+def select_countries_get():
+    return render_template("selectcountries.html")
